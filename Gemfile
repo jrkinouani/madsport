e1c686775a775484b473b57f07678ca2d59bc2eb
jean-rodriguez kinouani <kinouani@gmail.com>
2023-02-21 19:37:39 +0100

    Add contact form

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ gem "autoprefixer-rails"
 gem "font-awesome-sass", "~> 6.1"
 gem "simple_form", github: "heartcombo/simple_form"
 gem "cloudinary"
+gem 'mail_form'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get "/stage", to: "pages#stage"
   get "/collectif", to: "pages#collectif"
   get "/contact", to: "pages#contact"
+  resources :contacts, only: [:new, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
