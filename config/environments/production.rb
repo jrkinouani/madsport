b37bab7475206691f10260a6df893a743b3ebb61
jean-rodriguez kinouani <kinouani@gmail.com>
2023-02-22 14:09:25 +0100

    Fix SyntaxError config production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = {host: 'https://madsport.herokuapp.com/'}
-  action_action_mailer.delivery_method = :smtp
+  config_action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
