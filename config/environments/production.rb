19b21990010476c5a15d23819aef07889b3e4ab5
jean-rodriguez kinouani <kinouani@gmail.com>
2023-02-22 17:58:45 +0100

    fix sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,14 +92,16 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = {host: 'https://madsport.herokuapp.com/'}
-  config_action_mailer.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => "madsport.herokuapp.com",
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'madsport.herokuapp.com',
     :enable_starttls_auto => true
-}
+  }
 
 end
