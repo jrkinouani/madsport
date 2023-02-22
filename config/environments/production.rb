5a36796b831f840b040e43039cb86fdc5d14da78
jean-rodriguez kinouani <kinouani@gmail.com>
2023-02-22 19:32:47 +0100

    change key sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,7 +96,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
     :user_name => 'apikey',
-    :password => ENV['SENDGRID_PASSWORD'],
+    :password => Rails.application.secrets.sendgrid_api_key,
     :domain => "madsport.herokuapp.com",
     :address => 'smtp.sendgrid.net',
     :port => 587,
