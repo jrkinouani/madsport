6d5ab2ad920732cff10679886a00436fcc72fd02
jean-rodriguez kinouani <kinouani@gmail.com>
2023-02-22 14:14:41 +0100

    Create file smtp.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,16 +90,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = {host: 'https://madsport.herokuapp.com/'}
   config_action_mailer.delivery_method = :smtp
-
-  ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :porr           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
-    :enable_starttls_auto => true
-  }
+  config.action_mailer.default_url_options = {host: 'https://madsport.herokuapp.com/'}
 end
