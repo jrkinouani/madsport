d0876d7d24fde4fe9fd9736d21b533e433c7113b
jean-rodriguez kinouani <kinouani@gmail.com>
2023-03-08 17:35:23 +0100

    test send email in prod

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,10 +1,8 @@
-# config/initializers/smtp.rb
 ActionMailer::Base.smtp_settings = {
-  address: 'smtp.sendgrid.net',
-  port: 587,
   domain: 'beyondboxing.fr',
-  user_name: ENV['SENDGRID_USERNAME'],
-  password: ENV['SENDGRID_PASSWORD'],
-  authentication: :login,
-  enable_starttls_auto: true
+  address:        "smtp.sendgrid.net",
+  port:            587,
+  authentication: :plain,
+  user_name:      'apikey',
+  password:       ENV['SENDGRID_API_KEY']
 }
