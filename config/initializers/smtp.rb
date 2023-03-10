319511d8c7a34a9c2886b050c4ab686605251aa9
jean-rodriguez kinouani <kinouani@gmail.com>
2023-03-10 16:01:07 +0100

    change api key sendgrid

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -3,6 +3,6 @@ ActionMailer::Base.smtp_settings = {
   address:        "smtp.sendgrid.net",
   port:            587,
   authentication: :plain,
-  user_name: "apikey",
-  password: ENV['SENDGRID_API_KEY'],
+  user_name:      'apikey',
+  password:       Rails.application.credentials.dig(sendgrid: :api_key)
 }
