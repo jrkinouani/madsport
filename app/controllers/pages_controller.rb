7b4208580a05590d731e9e218ee741a4df04ba85
jean-rodriguez kinouani <kinouani@gmail.com>
2023-04-03 10:58:28 +0200

    Fix text and link homepage and navbar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,28 +1,33 @@
 class PagesController < ApplicationController
-  def home
-  end
-
-  def faq
-  end
-
-  def entreprise
-  end
-
-  def private
-  end
-
-  def formation
+  
+  def about
   end
 
   def collectif
   end
 
+  def contact
+  end
+
+  def entreprise
+  end
+
+  def faq
+  end
+
+  def formation
+  end
+
+  def home
+  end
+
+  def merci 
+  end
+
+  def private
+  end
+
   def tarifs
   end
 
-  def about
-  end
-
-  def contact
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get "/entreprise", to: "pages#entreprise"
   get "/faq", to: "pages#faq"
   get "/formation", to: "pages#formation"
+  get "/merci", to: "pages#merci"
   get "/private", to: "pages#private"
   get "/tarifs", to: "pages#tarifs"
   get "/thanks", to: "contacts#thanks"
